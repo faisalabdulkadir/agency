76a46bb65709b60a392bacf0fe73c99de60a782c
faisalabdulkadir <wizardfaisal@gmail.com>
2024-07-01 22:55:54 +0100

    designed the about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,44 @@
+import Image from "next/image";
+import styles from "./about.module.css";
+
 const AboutPage = () => {
   return (
-    <div>AboutPage</div>
-  )
-}
+    <div>
+      <div className={styles.container}>
+        <div className={styles.textContainer}>
+          <h2 className={styles.subTitle}>About Agency</h2>
+          <h1 className={styles.title}>
+            We create digital ideas that are bigger, bolder, braver and better
+          </h1>
+          <p className={styles.description}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+          <div className={styles.boxes}>
+            <div className={styles.box}>
+              <h1>10 K+</h1>
+              <p>Year of experience</p>
+            </div>
 
-export default AboutPage
+            <div className={styles.box}>
+              <h1>234 K+</h1>
+              <p>People reached</p>
+            </div>
+
+            <div className={styles.box}>
+              <h1>5 K+</h1>
+              <p>Services and plugins</p>
+            </div>
+          </div>
+        </div>
+        <div className={styles.imgContainer}>
+          <Image src="/about.png" fill alt="" className={styles.img}/>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutPage;
