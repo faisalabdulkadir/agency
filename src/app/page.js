526670f16b209ef916cf2e3270f253fb39df8da1
faisalabdulkadir <wizardfaisal@gmail.com>
2024-07-01 22:26:36 +0100

    designed the homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,25 @@
+import Image from "next/image";
+import styles from "./home.module.css";
 export default function Home() {
-  return <div>Home page</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Creative Thoughts Agency</h1>
+        <p className={styles.description}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+        <div className={styles.buttons}>
+          <button className={styles.button}>Learn More</button>
+          <button className={styles.button}>Contact</button>
+        </div>
+        <div className={styles.brands}>
+          <Image src="/brands.png" fill alt="" className={styles.brandImg} />
+        </div>
+      </div>
+      <div className={styles.imageContainer}>
+        <Image src="/hero.gif" fill className={styles.heroImg} />
+      </div>
+    </div>
+  );
 }
