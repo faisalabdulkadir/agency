5fe54cde660ff814c2cc658f5b157f9a4ffde7b3
faisalabdulkadir <wizardfaisal@gmail.com>
2024-07-01 22:27:11 +0100

    designed the footer

--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -1,5 +1,11 @@
+import styles from "./footer.module.css"
 const Footer = () => {
-  return <div>Footer</div>;
+  return <div className={styles.container}>
+    <div className={styles.logo}>MyStuff</div>
+    <div className={styles.text}>
+      MyStuff Creative Agency 	&#169; All rights reserved
+    </div>
+  </div>;
 };
 
 export default Footer;
