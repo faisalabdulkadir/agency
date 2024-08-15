303858f3b74d47bf9b0d686722ff95802d765f4a
faisalabdulkadir <wizardfaisal@gmail.com>
2024-08-15 14:12:51 +0100

    minor fixing to postcard

--- src/component/postCard/postCard.jsx
+++ src/component/postCard/postCard.jsx
@@ -3,7 +3,7 @@ import styles from "./postCard.module.css";
 import Link from "next/link";
 
 const PostCard = ({ post }) => {
-  console.log("post", post)
+
   return (
     <div className={styles.container}>
       <div className={styles.top}>
