f391f0b971b0dd6fd5af1a374302afae6da51d27
faisalabdulkadir <wizardfaisal@gmail.com>
2024-10-06 21:33:10 +0100

    minor tweek

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -1,12 +1,15 @@
+import { auth } from "@/lib/auth";
 import Links from "./link/Links";
 import styles from "./navbar.module.css";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const session = await auth();
+  console.log(session);
   return (
     <div className={styles.container}>
       <div className={styles.logo}>MyStuff</div>
       <div>
-        <Links />
+        <Links session={session} />
       </div>
     </div>
   );
