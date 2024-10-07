b0dc436b2ac24645b20921eb26224c9173b6d640
faisalabdulkadir <wizardfaisal@gmail.com>
2024-10-07 12:52:41 +0100

    created the register page

--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,7 +1,24 @@
-const AdminPage = () => {
-  return (
-    <div>AdminPage</div>
-  )
-}
+import { register } from "@/lib/action";
+import styles from "./register.module.css";
 
-export default AdminPage
+const RegisterPage = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <form action={register} className={styles.form}>
+          <input type="text" placeholder="username" name="username" />
+          <input type="text" placeholder="email" name="email" />
+          <input type="password" placeholder="password" name="password" />
+          <input
+            type="password"
+            placeholder="password again"
+            name="passwordRepeat"
+          />
+          <button>Register</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default RegisterPage;
