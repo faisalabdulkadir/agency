32e3f474ce5ad67d3ea254c1a6ee59dd7740b0f4
faisalabdulkadir <wizardfaisal@gmail.com>
2024-10-07 12:52:57 +0100

    created function for register

--- src/lib/action.js
+++ src/lib/action.js
@@ -1,9 +1,10 @@
 "use server";
 
 import { revalidatePath } from "next/cache";
-import { Post } from "./models";
+import { Post, User } from "./models";
 import { connectToDB } from "./util";
 import { signIn, signOut } from "./auth";
+import bcrypt from "bcrypt";
 
 export const addPost = async (formData) => {
   const { title, desc, userId, slug } = Object.fromEntries(formData);
@@ -40,4 +41,37 @@ export const handleLoginGithub = async () => {
 
 export const handleLogoutGithub = async () => {
   await signOut();
+};
+
+export const register = async (data) => {
+  const { username, email, password, passwordRepeat, img } =
+    Object.fromEntries(data);
+  console.log({ username, email, password, passwordRepeat, img });
+  if (password !== passwordRepeat) {
+    return "Password do not match";
+  }
+  try {
+    connectToDB();
+    const user = await User.findOne({ username });
+    if (user) {
+      return "User already exist in our record";
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+      img,
+    });
+    await newUser.save();
+    return "user saved to db";
+  } catch (error) {
+    console.log(error);
+    return { Error: "Something went wrong" };
+  }
+  //test if user already exists then notify us
+  //if user doesn't exist create a new user and save on the db
 };
