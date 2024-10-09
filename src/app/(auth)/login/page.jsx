e66f14f378161fa5bccdc4bd3d805a181c2fab96
faisalabdulkadir <wizardfaisal@gmail.com>
2024-10-09 23:15:06 +0100

    added error display on forms

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,11 +1,15 @@
-import { handleLoginGithub } from "@/lib/action";
+import { handleLoginGithub, login } from "@/lib/action";
 
-const LoginPage = async() => {
-  
+const LoginPage = async () => {
   return (
     <div>
       <form action={handleLoginGithub}>
         <button>Login with Github</button>
+      </form>
+      <form action={login}>
+        <input type="text" placeholder="username" name="username" />
+        <input type="password" placeholder="password" name="password" />
+        <button>Login with credentials</button>
       </form>
     </div>
   );
