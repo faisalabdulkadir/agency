c6509cdee9d4410eeec5951ae48bb934e677e806
faisalabdulkadir <wizardfaisal@gmail.com>
2024-10-09 23:13:57 +0100

    added component to register page

--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,21 +1,11 @@
-import { register } from "@/lib/action";
 import styles from "./register.module.css";
+import RegisterFormPage from "@/component/registerForm/registerFormPage";
 
 const RegisterPage = () => {
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
-        <form action={register} className={styles.form}>
-          <input type="text" placeholder="username" name="username" />
-          <input type="text" placeholder="email" name="email" />
-          <input type="password" placeholder="password" name="password" />
-          <input
-            type="password"
-            placeholder="password again"
-            name="passwordRepeat"
-          />
-          <button>Register</button>
-        </form>
+        <RegisterFormPage />
       </div>
     </div>
   );
